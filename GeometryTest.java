38675b17bd56ec3d65a5215d14bb69689f8bcbcc
Anna-Pushkina <79539640+Anna-Pushkina@users.noreply.github.com>
2021-04-15 17:23:18 +0300

    Create GeometryTest.java

---
+++ GeometryTest.java
@@ -0,0 +1,10 @@
+public class GeometryTest {
+  public static void main(String[] args) {
+       new Geometry().addShape(new Circle());
+       new Geometry().addShape(new Square());
+       new Geometry().addShape(new Rectangular());
+       new Geometry().addShape(new Star());
+       new Geometry().addShape(new Rhombus());
+
+  }
+}
